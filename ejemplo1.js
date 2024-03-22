4165639959227f9d56415ba91daf4a42ad7dbf91
GabrielVall3 <130763268+GabrielVall3@users.noreply.github.com>
2024-03-22 14:34:24 -0300

    cambio

---
+++ ejemplo1.js
@@ -0,0 +1,9 @@
+function factorial(numero){
+    var resultado; 
+    for(var i=0;i<=numero;i++){
+        var mult
+        resultado = resultado + i * numero;
+     }
+     return resultado;
+}
+console.log(factorial(10));
